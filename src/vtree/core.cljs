7b3a22d8dbc818d2e2973f5ae5796441c1ff2909
Paulus Esterhazy <pesterhazy@gmail.com>
2017-04-30 22:48:14 -0700

    WIP

--- src/vtree/core.cljs
+++ src/vtree/core.cljs
@@ -9,28 +9,16 @@
   (let [n (count a)]
     (= (take n a) (take n b))))
 
-#_(defn banana* [xs prefix]
-    (loop [xs xs
-           prefix prefix
-           acc []]
-      (let [e (take (inc (count prefix)) xs)]
-        (if (seq xs)
-          (let [[head tail] (split-with (fn [x] (prefix-of? e x)) xs)]
-            (recur tail prefix (conj acc (into [(last e)] (banana (rest ) e)))))
-          acc))))
-
 (defn banana [xs]
-  (prn [:banana xs])
   (loop [xs xs
          acc []]
     (if (empty? xs)
       acc
       (let [x (ffirst xs)
-            _ (prn [:ffirst x])
             [head tail] (split-with (partial prefix-of? [x]) xs)
             more (->> head (map next) (remove empty?))
             v (if (seq more)
-                (into [x] (map banana more))
+                (into [x] (banana more))
                 x)]
         (recur tail (conj acc v))))))
 
