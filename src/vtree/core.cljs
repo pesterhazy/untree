0a6a3260611b7eee362a40668c3de84b48b2500c
Paulus Esterhazy <pesterhazy@gmail.com>
2017-04-30 22:07:31 -0700

    WIP

--- src/vtree/core.cljs
+++ src/vtree/core.cljs
@@ -1,10 +1,30 @@
 (ns vtree.core
-  (:require [clojure.string]))
+  (:require [clojure.string]
+            [clojure.pprint :refer [pprint]]
+            [lumo.io]))
 
 (defn init [])
 
+(defn prefix-of? [a b]
+  (let [n (count a)]
+    (= (take n a) (take n b))))
+
+(defn banana [xs prefix]
+  (loop [[fst :as xs] xs
+         prefix prefix
+         acc []]
+    (if (seq xs)
+      (let [[[_ & rst :as head] tail] (split-with (fn [x] (prefix-of? fst x)) xs)]
+        (prn {:fst fst :head head :tail tail})
+        (recur tail prefix (conj acc {:node fst
+                                      :children (banana rst fst)})))
+      acc)))
+
 (defn run []
-  (prn [:run]))
+  (let [xs (->> (lumo.io/slurp "tree")
+                (clojure.string/split-lines)
+                (map #(clojure.string/split % "/")))]
+    (pprint (banana xs []))))
 
 (defn -main [& more]
   (init)
